d98f65eff1ad2d47b4f7e12515c63c3808b0d6d1
deepaksahoo9725 <deepaksahoo9725@gmail.com>
2016-09-27 01:03:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,11 @@
 console.log('Loaded!');
 //move the image
 var img = document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft + 5;
+    img.style.marginLeft = ,arginLeft +'px';
+}
 img.onclick = function(){
- img.style.marginLeft ='100px';
+    var interval = setInterval(moveRight,40); 
 };
